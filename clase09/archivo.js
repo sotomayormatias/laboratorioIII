26156cae2633de7d6c82fb3a2d453a9b6b1632c3
alumno <alumno>
2017-05-23 22:20:21 -0300

    typescript

---
+++ clase09/archivo.js
@@ -0,0 +1,9 @@
+var esSuperman = true;
+if (esSuperman) {
+    console.log("es superman");
+}
+var superman = "Superman";
+var batman = "Batman";
+var linternaVerde = "linterna verde";
+var mensaje = "los heroes son: " + superman + ", " + batman + ", " + linternaVerde + ".";
+alert(mensaje);
---
+++ clase09/archivo.ts
@@ -0,0 +1,13 @@
+let esSuperman:boolean = true;
+
+if(esSuperman){
+    console.log("es superman");
+}
+
+let superman:string = "Superman";
+let batman:string = "Batman";
+let linternaVerde:string = "linterna verde";
+
+let mensaje:string = `los heroes son: ${superman}, ${batman}, ${linternaVerde}.`;
+
+alert(mensaje);
