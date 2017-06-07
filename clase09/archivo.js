a4bcd3728a4c5c4110c3c083e923c904bc3513fc
alumno <alumno>
2017-06-06 22:04:00 -0300

    tsconfig.json mas cosas de typescript

--- clase09/archivo.js
+++ clase09/archivo.js
@@ -7,3 +7,16 @@ var batman = "Batman";
 var linternaVerde = "linterna verde";
 var mensaje = "los heroes son: " + superman + ", " + batman + ", " + linternaVerde + ".";
 alert(mensaje);
+var flash = {
+    nombre: "Barry Allen",
+    edad: 24,
+    poderes: ["Corre muy rápido", "Viaja en el tiempo"],
+    getNombre: function () {
+        return this.nombre;
+    }
+};
+var spiderman;
+var mutable;
+mutable = "hola guachin";
+mutable = 23;
+//# sourceMappingURL=archivo.js.map
--- clase09/archivo.ts
+++ clase09/archivo.ts
@@ -1,8 +1,15 @@
 let esSuperman:boolean = true;
 
+//Esto genera un tipo de dato (es similar al typeDef de C)
+type Heroe = {nombre:string, edad:number, poderes:string[], getNombre:()=>string};
+
 if(esSuperman){
     console.log("es superman");
 }
+
+// let superman:string = "Superman";
+// let batman:string = "Batman";
+// let linternaVerde:string = "linterna verde";
 
 let superman:string = "Superman";
 let batman:string = "Batman";
@@ -11,3 +18,20 @@ let linternaVerde:string = "linterna verde";
 let mensaje:string = `los heroes son: ${superman}, ${batman}, ${linternaVerde}.`;
 
 alert(mensaje);
+
+let flash: { nombre:string, edad:number, poderes:string[], getNombre:()=>string}= {
+    nombre: "Barry Allen",
+    edad: 24, 
+    poderes: ["Corre muy rápido", "Viaja en el tiempo"],
+    getNombre(){ 
+        return this.nombre;
+    } 
+};
+
+let spiderman: Heroe;
+
+
+let mutable : string | number;
+
+mutable = "hola guachin";
+mutable = 23;
